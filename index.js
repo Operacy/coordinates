deefcea6645410325fee054e30cc805429334214
Hans Skjerpen <hans.skjerpen@guardsystems.com>
2015-03-28 22:10:12 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+/**
+    Parses GIS coordinate pairs from strings
+
+    Usage:
+var coords  = require('coordinates'),
+    extract = coords.extract,
+    pair    = coords.pair;
+
+    var string = 'noise before 27.00N 87W and noise after';
+    var extract = extract(string);
+    var pair = pair(extract);
+
+    Formats from:
+    http://resources.arcgis.com/en/help/main/10.1/index.html#//001700000186000000
+
+    Degree-based formats:
+    ----------------------------------
+    DD (Decimal Degrees):
+
+    <latitude> <separator> <longitude>
+    latitude: [ + | - | N | S ] <DD.dd> [ + | - | N | S ]	DD = 00 - 90, dd = 00 - 99
+    longitude: [ + | - | E | W ] <DDD.dd> [ + | - | E | W ]	DDD = 0 til 180 & 0 til -180
+    separator: [space | / | \ | | | , ]
+
+    Latitude <DD.dd> and longitude <DDD.dd> values can be formatted as:
+    <degrees> [<decimal>] <fraction of degree> [<degree-mark>]
+
+    [+|-]*[N|n|S|s]*(\d{2,3})+(\.)*(\d)*[\u00B0]*[N|n|S|s]*([\s/])*[+|-]*[W|w|E|e]*(\d{2,3})*((\.)*(\d{2}))*[\u00B0]*[W|w|E|e]*
+*/
+var degrees = /(\d{2,3})+/;
+var decimal = /(\.)*/;
+var fraction_of_degree = /(\d)*/;
+var degree_mark = /[\u00B0]*/;
+var north_south = /[+|\-|N|n|S|s]*/;
+var east_west = /[+|\-|E|e|W|w]*/;
+var separator = /[\s|\/|\\|\||,]+/;
+
+var latitude = north_south.source + degrees.source + decimal.source + fraction_of_degree.source + degree_mark.source + north_south.source;
+var longitude = east_west.source + degrees.source + decimal.source + fraction_of_degree.source + degree_mark.source + east_west.source;
+
+var DD = new RegExp(latitude + separator.source + longitude);
+var number = new RegExp(/[1-9]+(\d)*(\.)*(\d)*/);
+
+var coordinates = {
+    DD: function (string) {
+        var x = DD.exec(string);
+        return (x && x[0])
+            ? {coords: x[0], format: 'DDD'}
+            : null;
+    },
+
+    DDM: function (string) { return null; },  // DDM (degree minutes)
+    DMS: function (string) { return null; },  // DMS (degree minute seconds)
+    UTM: function (string) { return null; },  // UTM (Universal Transverse Mercator)
+    MGRS: function (string) { return null; }, // (MGRS) Military Grid Reference System
+    //, Other formats in here.
+
+    extract: function (string) {
+        // try to match all different formats
+        return coordinates.DDM(string)
+            || coordinates.DMS(string)
+            || coordinates.UTM(string)
+            || coordinates.MGRS(string)
+            || coordinates.DD(string);  // Add other formats when needed
+    },
+
+    pair: function (str) {
+        // input string with coordinates
+        // output object with lat and lon
+        var pair = str && str.coords && str.coords.split(coordinates.patterns.separator);
+        lat = pair && number.exec(pair[0]) || undefined;
+        lon = pair && number.exec(pair[1]) || undefined;
+
+        return (lat && lon)
+            ? { lat: lat && Number(lat[0]), lon: lon && Number(lon[0]), format: str.format }
+            : undefined;
+    },
+
+    patterns: {
+        separator: separator
+    }
+};
+
+module.exports = coordinates;
---
+++ test/test.js
@@ -0,0 +1,62 @@
+var test = require('tape'),
+    coords  = require('../index.js'),
+    extract = coords.extract,
+    split   = coords.split,
+    pair    = coords.pair;
+
+var strings = {
+    'noise before 27.00N 87W and noise after': {
+        'extract': { coords: '27.00N 87W', format: 'DDD' },
+        'pair': { lat: 27, lon: 87, format: 'DDD' }
+    },
+    '27.00N/87W': {
+        'extract': { coords: '27.00N/87W', format: 'DDD' },
+        'pair': { lat: 27, lon: 87, format: 'DDD' }
+    },
+    '27.00N 087.00W': {
+        'extract': { coords: '27.00N 087.00W', format: 'DDD' },
+        'pair': { lat: 27, lon: 87, format: 'DDD' }
+    },
+    '27.0123n 087.002w': {
+        'extract': { coords: '27.0123n 087.002w', format: 'DDD' },
+        'pair': { lat: 27.0123, lon: 87.002, format: 'DDD' }
+    },
+    '27.0067N 087W': {
+        'extract': { coords: '27.0067N 087W', format: 'DDD' },
+        'pair': { lat: 27.0067, lon: 87, format: 'DDD' }
+    },
+    '+27.00 087W': {
+        'extract': { coords: '+27.00 087W', format: 'DDD' },
+        'pair': { lat: 27, lon: 87, format: 'DDD' }
+    },
+    '27N -87': {
+        'extract': { coords: '27N -87', format: 'DDD' },
+        'pair': { lat: 27, lon: 87, format: 'DDD' }
+    },
+    'N27.00 W087': {
+        'extract': { coords: 'N27.00 W087', format: 'DDD' },
+        'pair': { lat: 27, lon: 87, format: 'DDD' }
+    },
+    '27.05N/87.123W': {
+        'extract': { coords: '27.05N/87.123W', format: 'DDD' },
+        'pair': { lat: 27.05, lon: 87.123, format: 'DDD' }
+    },
+    '27.00°N 087.00°W': {
+        'extract': { coords: '27.00°N 087.00°W', format: 'DDD' },
+        'pair': { lat: 27, lon: 87, format: 'DDD' }
+    }
+};
+
+
+test('Testing extraction of coordinates from a bunch of different strings', function(t) {
+
+    t.plan(Object.keys(strings).length*2)
+
+    Object.keys(strings).forEach(function(str){
+        var extract = coords.extract(str);
+        t.deepEqual(extract, strings[str].extract, 'Extracting coords from '+str)
+
+        var pair = coords.pair(extract);
+        t.deepEqual(pair, strings[str].pair, 'Extracting coord pair from '+JSON.stringify(extract));
+    })
+});
